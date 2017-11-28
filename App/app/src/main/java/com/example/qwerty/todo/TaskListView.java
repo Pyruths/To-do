976a5db78701fc56056b6165375ff599bfc16931
robin phoeng <rpho6@student.monash.edu>
2017-11-28 19:20:59 +1100

    Context menu for recycler View
    Code is becoming messy, should clean up and also add tests

--- App/app/src/main/java/com/example/qwerty/todo/TaskListView.java
+++ App/app/src/main/java/com/example/qwerty/todo/TaskListView.java
@@ -4,11 +4,7 @@ import android.os.Bundle;
 import android.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.ActionMode;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -37,34 +33,8 @@ public class TaskListView extends Fragment {
     private static final String PARENT = "parent";
     private int parentID;
 
-    private ActionMode.Callback mActionCallback = new ActionMode.Callback() {
-        @Override
-        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
-            MenuInflater inflater = actionMode.getMenuInflater();
-            inflater.inflate(R.menu.task_bar,menu);
-            return true;
-        }
 
-        @Override
-        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
-            return false;
-        }
 
-        @Override
-        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
-            switch (menuItem.getItemId()){
-                case R.id.delete:
-                    deleteAll();
-                    return true;
-                default:
-                    return false;
-            }
-        }
-
-        @Override
-        public void onDestroyActionMode(ActionMode actionMode) {
-        }
-    };
     public TaskListView() {
         // Required empty public constructor
     }
@@ -91,8 +61,6 @@ public class TaskListView extends Fragment {
         super.onCreate(savedInstanceState);
 
         parentID = getArguments().getInt(PARENT,-1);
-        inflateTasks();
-
     }
 
     @Override
@@ -107,7 +75,7 @@ public class TaskListView extends Fragment {
         mLayoutManager = new LinearLayoutManager(getActivity());
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new TodoItemAdapter(mTasks);
+        mAdapter = new TodoItemAdapter(getActivity().getApplicationContext(),parentID);
         mRecyclerView.setAdapter(mAdapter);
 
         return rootView;
@@ -119,44 +87,8 @@ public class TaskListView extends Fragment {
         outState.putInt(PARENT,parentID);
     }
 
-    private void inflateTasks(){
-        TaskDataBase db = TaskDataBase.getDatabase(getActivity().getApplicationContext());
-        mTasks = null;
-        if (parentID < 0){
-            //get all
-            try{
-                mTasks = new GetTasks(db).execute().get();
-            } catch (InterruptedException | ExecutionException e){
-                e.printStackTrace();
-            }
-        }
-        else
-        {
-            try{
-                mTasks = new GetSubTasks(db).execute(parentID).get();
-            } catch (InterruptedException | ExecutionException e){
-                e.printStackTrace();
-            }
-        }
-    }
-    public void update(){
-        inflateTasks();
-        mAdapter.setTasks(mTasks);
-
-    }
-
     public void deleteAll(){
-        ArrayList<Task> h = new ArrayList<>();
-        for (Task t : mTasks){
-            if(t.isSelected()){
-                h.add(t);
-            }
-        }
-        // Turn it into an array that is processable by the Async Task
-        Task[] tasklist = new Task[h.size()];
-        TaskDataBase db = TaskDataBase.getDatabase(getActivity().getApplicationContext());
-        new DeleteTasks(db).execute(h.toArray(tasklist));
-        update();
+
     }
 
 
--- App/app/src/main/java/com/example/qwerty/todo/TodoItemAdapter.java
+++ App/app/src/main/java/com/example/qwerty/todo/TodoItemAdapter.java
@@ -1,28 +1,45 @@
 package com.example.qwerty.todo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
+import com.example.qwerty.todo.DataBase.AsyncTasks.DeleteTasks;
+import com.example.qwerty.todo.DataBase.AsyncTasks.GetSubTasks;
+import com.example.qwerty.todo.DataBase.AsyncTasks.GetTasks;
 import com.example.qwerty.todo.DataBase.Task;
+import com.example.qwerty.todo.DataBase.TaskDataBase;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 
 /**
  * Created by Qwerty on 24/11/2017.
  * To be used in conjunction with a Recycler View
- * This allows for viewing and selection of a list of tasks.
+ * This allows for viewing and selection of a list of mTasks.
  */
 
 public class TodoItemAdapter extends RecyclerView.Adapter<TodoItemAdapter.ViewHolder>{
-    private Task[] tasks;
+    private Task[] mTasks;
+    private ActionMode mActionMode;
+    private Context appContext;
+    private ArrayList<Task> mSelectedTasks;
+    private int parentID;
+
 
     class ViewHolder extends RecyclerView.ViewHolder{
         CardView mCard;
@@ -39,8 +56,11 @@ public class TodoItemAdapter extends RecyclerView.Adapter<TodoItemAdapter.ViewHo
 
     }
 
-    TodoItemAdapter(Task[] tasks){
-        this.tasks = tasks;
+    TodoItemAdapter(Context appContext, int parentID){
+        mSelectedTasks = new ArrayList<>();
+        this.parentID = parentID;
+        this.appContext = appContext;
+        inflateTasks();
 
     }
     @Override
@@ -53,10 +73,13 @@ public class TodoItemAdapter extends RecyclerView.Adapter<TodoItemAdapter.ViewHo
 
     @Override
     public void onBindViewHolder(TodoItemAdapter.ViewHolder holder, int position) {
-        final int taskID = tasks[position].getId();
+        final int taskID = mTasks[position].getId();
         final int fposition = position;
-        holder.mText.setText(tasks[position].getText());
-        holder.mDate.setText(tasks[position].getExpiration().toString());
+        Task t = mTasks[position];
+        holder.mText.setText(t.getText());
+        holder.mDate.setText(t.getExpiration().toString());
+
+        // Set the card selection.
         holder.mCard.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -65,27 +88,108 @@ public class TodoItemAdapter extends RecyclerView.Adapter<TodoItemAdapter.ViewHo
                 v.getContext().startActivity(intent);
             }
         });
-
+        // Set the Checkbox
+        holder.mSelect.setChecked(t.isSelected());
+        holder.mSelect.setBackgroundColor(t.isSelected()? Color.CYAN: Color.WHITE);
         holder.mSelect.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Task current = tasks[fposition];
+                Task current = mTasks[fposition];
                 current.toggle(!current.isSelected());
 
+                if (current.isSelected()){
+                    mSelectedTasks.add(current);
+                    if (mActionMode == null){
+                        mActionMode = ((AppCompatActivity)v.getContext()).startActionMode(mActionCallback);
+                    }
+                }
+                else
+                {
+                    mSelectedTasks.remove(current);
+                    if (mSelectedTasks.size() == 0){
+                        mActionMode.finish();
+                    }
+                }
                 v.setBackgroundColor(current.isSelected()? Color.CYAN: Color.WHITE);
             }
         });
     }
 
+
     @Override
     public int getItemCount() {
-        return tasks.length;
+        return mTasks.length;
     }
 
-    public void setTasks(Task[] tasks) {
-        this.tasks = tasks;
+    /**
+     * Deletes all selected tasks via the user
+     */
+    private void deleteAll(){
+        ArrayList<Task> h = new ArrayList<>();
+        for (Task t : mTasks){
+            if(t.isSelected()){
+                t.toggle(false);
+                h.add(t);
+            }
+        }
+
+        TaskDataBase db = TaskDataBase.getDatabase(appContext);
+        // Turn it into an array that is processable by the Async Task
+        new DeleteTasks(db).execute(h.toArray(new Task[h.size()]));
+    }
+
+    private void inflateTasks(){
+        TaskDataBase db = TaskDataBase.getDatabase(appContext);
+        mTasks = null;
+        if (parentID < 0){//get all
+            try{
+                mTasks = new GetTasks(db).execute().get();
+            } catch (InterruptedException | ExecutionException e){
+                e.printStackTrace();
+            }
+        }
+        else
+        {
+            try{
+                mTasks = new GetSubTasks(db).execute(parentID).get();
+            } catch (InterruptedException | ExecutionException e){
+                e.printStackTrace();
+            }
+        }
         notifyDataSetChanged();
     }
 
 
+    private ActionMode.Callback mActionCallback = new ActionMode.Callback() {
+        @Override
+        public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+            MenuInflater inflater = actionMode.getMenuInflater();
+            inflater.inflate(R.menu.task_bar,menu);
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+            return false;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+            switch (menuItem.getItemId()){
+                case R.id.delete:
+                    deleteAll(); // delete
+                    inflateTasks(); // refresh
+                    actionMode.finish(); // finish
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode actionMode) {
+            // Deselect everything
+            mActionMode = null;
+        }
+    };
 }
